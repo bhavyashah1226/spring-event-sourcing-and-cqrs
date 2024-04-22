9ebe41943ae619f20c290a72b041651bfc9eb98e
bhavyashah1226 <43812628+bhavyashah1226@users.noreply.github.com>
2024-04-22 09:42:32 +0000

    API test (Using API Spec) generated by RoostGPT
    Using AI Model gpt-4

--- commands/src/main/java/com/baeldung/write/commands/CreateLead.java
+++ commands/src/main/java/com/baeldung/write/commands/CreateLead.java
@@ -6,54 +6,55 @@ import com.baeldung.infra.command.Command;
 
 public class CreateLead extends Command {
 
-    @NotNull
-    private String name;
+	@NotNull
+	private String name;
 
-    public CreateLead() {
-        super();
-    }
+	public CreateLead() {
+		super();
+	}
 
-    public CreateLead(final String name) {
-        super();
+	public CreateLead(final String name) {
+		super();
 
-        this.name = name;
-    }
+		this.name = name;
+	}
 
-    //
+	//
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+	public void setName(final String name) {
+		this.name = name;
+	}
 
-    //
+	//
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(final Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        final CreateLead other = (CreateLead) obj;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        return true;
-    }
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final CreateLead other = (CreateLead) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		}
+		else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 
 }
